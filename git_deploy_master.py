82b1f071d0abc1605e88714ebf81cb018699242e
Mauricio Azeredo <63761189+mauriciowebme@users.noreply.github.com>
2022-07-26 20:46:13 -0300

    ok

--- git_deploy_master.py
+++ git_deploy_master.py
@@ -1,15 +1,14 @@
 # pip install GitPython
 import os
-from git import Repo
-os.system('cls')
+import subprocess
 
+teste = subprocess.run([
+        "git"
+        ,"branch"
+        ]
+        #, stderr = subprocess.PIPE
+        , capture_output=True
+        , text=True
+        , shell=True)
+print(teste.stdout)
 base_dir = os.path.dirname(__file__)+'\\.git'
-repo = Repo(base_dir)
-assert not repo.bare
-
-git = repo.git
-git.checkout('HEAD', "my_new_branch")         # create a new branch
-git.checkout('HEAD', b="my_new_branch2")         # create a new branch
-git.branch('another-new-one')
-git.branch('-D', 'another-new-one')             # pass strings for full control over argument order
-git.for_each_ref()                              # '-' becomes '_' when calling it
