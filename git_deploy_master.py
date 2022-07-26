7f858a05b4ef32ad5844fcc39d8efe257a292f29
Mauricio Azeredo <63761189+mauriciowebme@users.noreply.github.com>
2022-07-26 20:23:42 -0300

    ok

--- git_deploy_master.py
+++ git_deploy_master.py
@@ -6,3 +6,10 @@ os.system('cls')
 base_dir = os.path.dirname(__file__)+'\\.git'
 repo = Repo(base_dir)
 assert not repo.bare
+
+git = repo.git
+git.checkout('HEAD', "my_new_branch")         # create a new branch
+git.checkout('HEAD', b="my_new_branch2")         # create a new branch
+git.branch('another-new-one')
+git.branch('-D', 'another-new-one')             # pass strings for full control over argument order
+git.for_each_ref()                              # '-' becomes '_' when calling it
