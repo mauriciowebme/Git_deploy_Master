8ffcf0cc059f356ab89e4cb13aca2b2fdc0fe96e
Mauricio Azeredo <63761189+mauriciowebme@users.noreply.github.com>
2022-07-26 21:22:05 -0300

    ok

--- git_deploy_master.py
+++ git_deploy_master.py
@@ -1,22 +1,22 @@
 # pip install GitPython
 import os
 import subprocess
-base_dir = os.path.dirname(__file__)
-retorno = subprocess.run([
-        "cd", base_dir
-        ]
-        #, stderr = subprocess.PIPE
-        , capture_output=True
-        , text=True
-        , shell=True)
-retorno = subprocess.run([
-        "dir"
-        ]
-        #, stderr = subprocess.PIPE
-        , capture_output=True
-        , text=True
-        , shell=True)
-print(retorno.stdout)
+
+def Comandos(comandos_list = []):
+        base_dir = os.path.dirname(__file__)
+        retorno = subprocess.run([
+                comandos_list
+                ]
+                #, stderr = subprocess.PIPE
+                , capture_output=True
+                , text=True
+                , shell=True)
+        return retorno.stdout
+
+teste = Comandos(['dir'])
+
+print(teste)
+
 retorno = subprocess.run([
         "git", "pull", "origin", "master"
         ]
